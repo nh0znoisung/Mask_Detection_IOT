3f74683a3622bdf35a32b0feab5917d72dca26dc
nh0znoisung <tuanbo2001@gmail.com>
2022-03-24 17:44:02 +0700

    Push gitlog

--- backend/models/RequestModel.js
+++ backend/models/RequestModel.js
@@ -4,8 +4,10 @@ import mongoose from "mongoose";
 const schema = new mongoose.Schema({
     subject: Number, //0: Admin, 1: AI
     action: Number, //0: Open door, 1: Close door, 2: Turn on bulb, 3: Turn off bulb
+    state: Number, // 0: In queue, 1: Pending, 2: Success, 2: Failed
 }, 
 {timestamps: true}
 );
 
 export const RequestModel = mongoose.model('request', schema);
+
